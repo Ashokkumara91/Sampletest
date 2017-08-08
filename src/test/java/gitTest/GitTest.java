51149f3c6c77f4239e453874fb038d4a4ddc99d1
Ashokkumara91 <ashoka91ece@gmail.com>
2017-08-08 10:15:15 -0700

    third commit

--- src/test/java/gitTest/GitTest.java
+++ src/test/java/gitTest/GitTest.java
@@ -15,7 +15,7 @@ public class GitTest {
 	@Test
 	public void logintest() throws IOException{
 		WebDriver wd = new FirefoxDriver();
-		wd.get("https://www.google.co.in/");
+		wd.get("https://www.google.com/");
 		File src = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
 	FileUtils.copyFile(src, new File("C:\\Selenium\\GitTest\\Screenshot\\google.jpg"));
 	}
